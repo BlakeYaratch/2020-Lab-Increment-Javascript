97db71a9a4a817f0a166d569463600ee34c4aee5
Yaratch_Blake <Yaratch_Blake@student.mahoningctc.com>
2020-12-02 11:20:09 -0500

    first commit

---
+++ lab1.js
@@ -0,0 +1,16 @@
+var secondsInAMinute = 60;
+var minutesInAHour = 60;
+var hoursInADay = 24;
+var daysInAYear = 365;
+
+var secondsInAHour=secondsInAMinute*minutesInAHour;
+document.writeln("The number of seconds in an hour is" + secondsInAHour);
+document.writeln("<br>");
+var secondsInADay=secondsInAHour*hoursInADay;
+document.writeln("The number of seconds in a day is " +secondsInADay);
+document.writeln("<br>");
+var secondsInAYear=secondsInADay*daysInAYear;
+document.writeln("The number of seconds in a year is " + secondsInAYear);
+document.writeln("<br>");
+document.writeln("<br>");
+document.writeln("Coppied by Blake Yaratch");
---
+++ lab1_2.js
@@ -0,0 +1,23 @@
+var numOfApples=10;
+document.writeln("The number of apples I have is "+ numOfApples + "<br>")
+numOfApples++
+document.writeln("The number of apples I have is "+ numOfApples + "<br>")
+numOfApples++
+document.writeln("The number of apples I have is "+ numOfApples + "<br>")
+numOfApples++
+document.writeln("The number of apples I have is "+ numOfApples + "<br>")
+numOfApples++
+document.writeln("The number of apples I have is "+ numOfApples + "<br>")
+numOfApples++
+document.writeln("The number of apples I have is "+ numOfApples + "<br>")
+numOfApples++
+document.writeln("<br><br>")
+var numOfNinjas=100;
+document.writeln("There are " +numOfNinjas+" ninjas in the room. <br>")
+document.writeln("There are "+ --numOfNinjas +" ninjas in the room. <br>")
+document.writeln("There are "+ --numOfNinjas +" ninjas in the room. <br>")
+document.writeln("There are "+ --numOfNinjas +" ninjas in the room. <br>")
+document.writeln("There are "+ --numOfNinjas +" ninjas in the room. <br>")
+document.writeln("There are "+ --numOfNinjas +" ninjas in the room. <br><br>")
+
+document.writeln("Done all on his own Blake Yaratch")
